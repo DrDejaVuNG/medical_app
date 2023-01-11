1af2dad79d41d9d7d160208ad91da9213ff50316
DrDejaVu <drdejavu08@gmail.com>
2023-01-11 03:02:40 +0100

    widget tree

---
+++ lib/widget_tree.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:medical_app/frontend/utils/bottom_nav.dart';
+import 'package:medical_app/frontend/views/auth/auth.dart';
+import 'package:medical_app/frontend/views/welcome/welcome.dart';
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        // user is logged in
+        if (snapshot.hasData) {
+          return const BottomNav();
+        }
+
+        // user not logged in
+        else {
+          return const WelcomeView();
+        }
+      },
+    );
+  }
+}
